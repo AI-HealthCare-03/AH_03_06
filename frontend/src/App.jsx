5669b23a88ffe782ba64209ab0b84474c810990e
Lunarian0928 <thanks092834@gmail.com>
2026-05-19 13:22:57 +0900

    feat: MyPage 및 라우터 업데이트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/auth/Login'
 import Register from './pages/auth/Register'
 import AuthCallback from "./pages/auth/AuthCallback.jsx";
 import Home from './pages/dashboard/Home'
+import MyPage from './pages/user/MyPage'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/user" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   )
